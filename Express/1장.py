9c51dd43f643885cc6c492688df72e021fd35132
Ray Lee <wooo519@naver.com>
2022-10-19 23:00:47 +0900

    1장

---
+++ Express/1장.py
@@ -0,0 +1,63 @@
+#1장 Progamming
+
+#01
+print("환영합니다.")
+print("파이썬의 세계로 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+#02
+print("+==============+")
+print("|    홍길동      |")
+print("+==============+")
+
+#03
+print(1+2+3)
+
+#04
+print("1+2+3을 계산하면 ",1+2+3,"입니다.")
+
+#05
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.bye()
+
+#06
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(300)
+t.left(90)
+t.forward(200)
+
+#07
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.width(10)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+#08
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("blue")
+t.forward(100)
+
+#09
+import turtle
+t = turtle.Turtle()
+t.shape("square")
+t.forward(100)
---
+++ Express/README.md
@@ -0,0 +1,1 @@
+# 파워유저를 위한 파이썬 express 연습문제 programming
